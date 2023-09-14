e965fc0f6effdff2eec497305b830f890500f03f
lokesh bhatt <lokesh.bhatt@tifr.res.in>
2023-09-14 12:13:16 +0200

    first

--- HitAnalysis/scripts/runloop.sh
+++ HitAnalysis/scripts/runloop.sh
@@ -2,8 +2,11 @@
 
 #inputdir=/home/idas/t3store3/root_files/HGCAL_Geometry/SimOut/DeltaPt/Extended2026D86
 #inputdir=/eos/user/i/idas/SimOut/DeltaPt/Extended2026D86
-inputdir=/eos/user/p/psuryade/SimOut/DeltaPt/Extended2026D86_25
-pydir=$PWD/ReadSimResult/SimTrackAna/python
+#inputdir=/eos/user/p/psuryade/SimOut/DeltaPt/Extended2026D86_25
+inputdir=/eos/user/l/lbhatt/MuDeltaPt100GeV
+
+#pydir=$PWD/ReadSimResult/SimTrackAna/python
+pydir=/afs/cern.ch/user/l/lbhatt/private/cmssw2/CMSSW_12_1_0_pre2/src/ReadSimResult/SimTrackAna/python
 for i in `seq 0 9`
 do
   echo Processing loop $i with file step1_${i}.root
@@ -14,13 +17,13 @@ do
   if [ -f $PWD/step1.root ] ; then
       rm $PWD/step1.root
   fi
-  ln -s $inputdir/step1_${i}.root $PWD/step1.root 
+#  ln -s $inputdir/step1_${i}.root $PWD/step1.root 
   #cmsRun $pydir/CellHitSum_cfg.py #-n 4
-  cmsRun $pydir/SimHit_cfg.py #-n 4
-  mv geantoutput.root geantoutput_${i}.root
+  cmsRun $pydir/CellHitSum_cfg.py  iter=$i  geometry=D86 #-n 4 
+#  mv geantoutput.root geantoutput_${i}.root
 done
 
-ls $PWD/geantoutput_*.root > /tmp/idas/fl.txt
-source ~/scripts/addhisto_file.sh /tmp/idas/fl.txt
+#ls $PWD/geantoutput_*.root > /tmp/idas/fl.txt
+#source ~/scripts/addhisto_file.sh /tmp/idas/fl.txt
 #rm geantoutput_*.root
-mv histo_merged.root geantoutput.root
+#mv histo_merged.root geantoutput.root
